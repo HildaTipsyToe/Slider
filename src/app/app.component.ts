0e3c8ef84b06de74984fe3711b5f46850140313a
Jeppe <88774896+HildaTipsyToe@users.noreply.github.com>
2023-04-20 14:23:52 +0200

    everything

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -8,3 +9,44 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'sliderBaseLine';
 }
+
+
+const track = document.getElementById('image-track')
+
+  window.onmousedown = e => (document.getElementById('image-track').dataset['mouseDownAt'] = e.clientX);
+  window.onmouseup = () => {
+    document.getElementById('image-track').dataset['mouseDownAt'] = "0";
+    document.getElementById('image-track').dataset[`prevPercentage`] = document.getElementById('image-track').dataset[`percentage`];
+  };
+  
+  window.onmousemove = e => {
+  if(parseInt(document.getElementById('image-track').dataset['mouseDownAt']) === 0)return;
+  
+    const mouseDelta = parseFloat(document.getElementById('image-track').dataset['mouseDownAt']) - e.clientX,
+      maxDelta = window.innerWidth / 2;
+  
+    var percentage= ( mouseDelta / maxDelta) * -100,
+    nextPercentage = Math.max(Math.min(parseFloat(document.getElementById('image-track').dataset['prevPercentage']) + percentage,0), -100);
+    //nextPercentageImage = Math.max(Math.min((parseFloat(document.getElementById('image-track').dataset['prevPercentage']) + percentage)/2,0), -100);
+  
+    ((document.getElementById('image-track').dataset['percentage'] as any) as number) = nextPercentage;
+  
+    document.getElementById('image-track').animate({
+      transform: `translate(${nextPercentage}%, -50%)`,
+    }, {
+      duration: 1200,
+      fill: "forwards"
+    });
+  
+    const tester = document.getElementById('image-track').getElementsByClassName("image");
+    console.log(nextPercentage, "NextPercentageImage")
+    console.log(nextPercentage, "nextPercentage")
+    Array.from(tester).forEach((image) => {
+      image.animate({
+        objectPosition: `${nextPercentage + 100}% center`,
+      },{
+        duration: 1200,
+        fill: "forwards"
+      })
+    })
+  };
--- src/main.ts
+++ src/main.ts
@@ -10,3 +10,45 @@ if (environment.production) {
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
+
+
+  // const track = document.getElementById('image-track')
+
+  // window.onmousedown = e => (track.dataset['mouseDownAt'] = e.clientX);
+  
+  // window.onmouseup = () => {
+  // track.dataset['mouseDownAt'] = "0";
+  // track.dataset[`prevPercentage`] = track.dataset[`percentage`];
+  // };
+  
+  // window.onmousemove = e => {
+  // if(parseInt(track.dataset['mouseDownAt']) === 0)return;
+  
+  //   const mouseDelta = parseFloat(track.dataset['mouseDownAt']) - e.clientX,
+  //     maxDelta = window.innerWidth / 2;
+  
+  //   var percentage= ( mouseDelta / maxDelta) * -100,
+  //   nextPercentage = Math.max(Math.min(parseFloat(track.dataset['prevPercentage']) + percentage,0), -175),
+  //   nextPercentageImage = Math.max(Math.min((parseFloat(track.dataset['prevPercentage']) + percentage)/2,0), -100);
+  
+  //   ((track.dataset['percentage'] as any) as number) = nextPercentage;
+  
+  //   track.animate({
+  //     transform: `translate(${nextPercentage}%, -50%)`,
+  //   }, {
+  //     duration: 1200,
+  //     fill: "forwards"
+  //   });
+  
+  //   const tester = track.getElementsByClassName("image");
+  //   console.log(nextPercentageImage, "NextPercentageImage")
+  //   console.log(nextPercentage, "nextPercentage")
+  //   Array.from(tester).forEach((image) => {
+  //     image.animate({
+  //       objectPosition: `${nextPercentageImage + 100}% center`,
+  //     },{
+  //       duration: 1200,
+  //       fill: "forwards"
+  //     })
+  //   })
+  // };
